7ed1a63268c776a247b9fa968f333a5e007be2a9
Piero Orderique <porderiq@mit.edu>
2023-07-09 18:45:03 -0700

    Make cli more informative

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from src.jarvis import JARVIS
+from src import JARVIS
 
 bot = JARVIS()
 bot.start_listening()
@@ -6,6 +6,7 @@ bot.start_listening()
 while True:
     command = input("Enter a command: quit, start, stop, history: ")
     if command == "quit":
+        print("Terminating main thread...")
         break
     elif command == "start":
         bot.start_listening()
@@ -17,3 +18,4 @@ while True:
         print("Invalid command.")
 
 bot.stop_listening()
+print("Program terminated.")
